bed12b2dd25a1193853347bb5914528daa7398de
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-07-05 23:15:55 +0100

    Add WHERE clause to booking query for filtering by date and price

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,4 @@
+EXPLAIN ANALYZE
 SELECT
     b.booking_id,
     b.start_date,
@@ -9,4 +10,7 @@ SELECT
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON pay.booking_id = b.booking_id;
+LEFT JOIN payments pay ON pay.booking_id = b.booking_id
+WHERE
+    b.start_date >= '2025-07-01'
+    AND b.total_price > 500;
